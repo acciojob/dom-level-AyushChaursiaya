9f9b359bacd04672041b0264ec74572bcad1fa87
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-02 13:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 //your JS code here. If required.
 
-document.addEventListener("DOMContentLoaded", function() {
-    var element = document.getElementById('level');
-    var level = 1;
-    
-    // Traverse through the parent elements until reaching the root html element
-    while (element.parentElement) {
-        level++;
-        element = element.parentElement;
-    }
-    
-    alert("The level of the element is: " + level);
+addEventListner("DOMContentLoaded", () => {
+	const element = document.getElementById("level");
+	let count = 0;
+	while (element.parentElement) {
+		count++;
+		element = element.perentElement;
+	}
+	
+	alert("The level of the element is: ", count);
 });
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var element = document.getElementById('level');
+//     var level = 1;
+    
+//     // Traverse through the parent elements until reaching the root html element
+//     while (element.parentElement) {
+//         level++;
+//         element = element.parentElement;
+//     }
+    
+//     alert("The level of the element is: " + level);
+// });
